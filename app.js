d4634b2f18f973b32b0136d1054b3ec0991b0248
junior-dev <64504551+dopdev-del@users.noreply.github.com>
2023-03-06 12:37:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const boton = document.getElementById("botoncontacto");
 const email = "juniornova90@gmail.com";
 
 boton.addEventListener("click", () =>{
-  alert("Mi correo electronico para contacto es " + email)
+  alert("Correo para contacto" + email + " gracias por intersarte en mi perfil, estare respondiendo tu correo tan pronto como me llegue")
 })
