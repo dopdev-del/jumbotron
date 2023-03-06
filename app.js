e596acbfd62137799b7a8d326318bc90d7f33e0d
junior-dev <64504551+dopdev-del@users.noreply.github.com>
2023-03-06 12:26:39 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("botoncontacto");
+const email = "juniornova90@gmail.com";
+
+boton.addEventListener("click", () =>{
+  alert("Mi correo electronico para contacto es " + email)
+})
